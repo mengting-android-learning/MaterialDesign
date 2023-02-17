1cb15e2d8bc3078c2cace8eee2b0bda61c010bf6
Rachel-oc <mengting.xie@thoughtworks.com>
2023-02-17 11:31:09 +0800

    [feat]:add click listener to the bottom bar navigation icon

--- app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.materialdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.materialdesign.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.bottomAppBar.setNavigationOnClickListener { Toast.makeText(this,"you clicked",Toast.LENGTH_LONG).show() }
     }
 }
